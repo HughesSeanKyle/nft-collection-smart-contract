2ed4b517a78bb705c8dde489402c2b721297dc52
HughesSeanKyle <khughessean@yahoo.com>
2022-10-10 16:24:43 +0200

    Config, Compile and deploy

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+/*
+    Address: 0xD5357C7235192a870A2720C77EA6de38905b6eF0
+    Etherscan: https://goerli.etherscan.io/address/0xD5357C7235192a870A2720C77EA6de38905b6eF0
+*/
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IWhitelist.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config({ path: '.env' });
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+	solidity: '0.8.4',
+	networks: {
+		goerli: {
+			url: QUICKNODE_HTTP_URL,
+			/*
+        Note pvt key is an array here as your pvt key can have multiple derived public addresses. Hence an array
+      */
+			accounts: [PRIVATE_KEY],
+		},
+	},
 };
